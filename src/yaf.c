e1e97aee06b5bde75ed1a5612b6bae1fb0c9d87f
jadeneyer <jadenmeyer@live.com>
2025-12-29 00:43:20 -0700

    general structure/linux tests

---
+++ src/yaf.c
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <sys/sysinfo.h>
+#include <sys/utsname.h>
+
+// print out the time very ugly should change soon
+void parse_uptime(long value) {
+
+  int days = value / 86400; // days
+  int hours = value / 3600; // hours
+  if (days > 0) {
+    hours -= days * 24;
+  }
+  printf("%d d %d h\n", days, hours);
+}
+
+int main(int argc, char *argv[]) {
+
+  // add documentations soon here
+  // eventually some custom stuff
+  if (argc > 0 && argc < 2) {
+    printf("var = %d\n", 3);
+  }
+
+  // static in case i decide to expand out
+  static struct utsname info;
+  static struct sysinfo system;
+
+  uname(&info); // fill info struct
+  sysinfo(&system);
+
+  // TODO:
+  // create a new file that parses the CPU information
+  // create a new file to print out a picture
+  // create a new file to get GPU information
+
+  // want this to be a double value
+  printf("%ld/%ld %s\n", system.freeram / 1000000000,
+         system.totalram / 1000000000, "RAM");
+  // get the uptime
+  // printf("%ld\n", system.uptime);
+  parse_uptime(system.uptime);
+  // get this to work maybe through /proc
+  printf("%s\n", info.sysname);
+  // get the kernel version
+  printf("%s\n", info.release);
+  return 0;
+}
